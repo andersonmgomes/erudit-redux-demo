fedbe1cd13789e5588b33be2c8196bdffb2390ac
Anderson Martins <andersonmg@gmail.com>
2023-04-27 17:37:40 -0300

    two panels with scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import MessageList  from "./features/message/MessageList";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import MessageList from './features/message/MessageList';
 
 function App() {
-  return (
-    <div>
-        <MessageList />
-    </div>
-  );
+    return (
+        <div className="app-container d-flex h-100">
+            <div className="message-list-wrapper">
+                <MessageList />
+            </div>
+            <div className="main-panel-wrapper flex-grow-1">
+                <h2>Main Panel</h2>
+            </div>
+        </div>
+    );
 }
 
 export default App;
