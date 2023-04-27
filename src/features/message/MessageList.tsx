1fd3dc0a38158f297d64b3a1f7ea75acf5d4d57f
Anderson Martins <andersonmg@gmail.com>
2023-04-27 11:29:10 -0300

    MessageList component

---
+++ src/features/message/MessageList.tsx
@@ -0,0 +1,11 @@
+function MessageList() {
+  return (
+    <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+    </ul>
+  );
+}
+
+export default MessageList;
